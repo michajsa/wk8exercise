2b0fe04d74dd3f056952bcc41478da2ab7db3b52
Michael Sanchez <michajsa@gene.com>
2017-10-02 15:17:21 -0700

    wk8exercise

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var element = document.getElementById('box')
+element.textContent = 'SLEEEEEEEPPP!'
+
+var newH1 = document.getElementById('newH1');
+
+function getValue() {
+  var input = document.getElementById('input');
+  console.log(input.value);
+}
+
+var myArray = ['uno', 'dos', 'tres', 'cuarto', 'cinco'];
+for (var i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+};
+
+// var gloves = {
+//   color: 'Red',
+//   size: 'large',
+//   feel: 'comfy',
+// };
+//
+// var shoes = {
+//   color: 'Purple',
+//   size: 'large',
+//   feel: 'comfy AF',
+// };
+//
+// var shirt = {
+//   color: 'Black',
+//   size: 'Extra large',
+//   feel: 'ok',
+// }
+
+// var pants = {
+//   color: 'Blue',
+//   size: 'x-large',
+//   feel: 'comfy',
+// }
+
+// for(var i = 0; i < )
